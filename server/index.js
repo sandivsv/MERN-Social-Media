30866ed19e6dc8aaada0af378529f04be029c1f4
Sandivsv <sandeepkmr7379@gmail.com>
2023-01-29 13:11:23 +0530

    post schema section done

--- server/index.js
+++ server/index.js
@@ -8,9 +8,16 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
 import { register } from "./controllers/auth.js";
+import { createPost } from "./controllers/posts.js";
+import { verifyToken } from "./middleware/auth.js";
+
 import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
+import postRoutes from "./routes/posts.js";
+
+
 
 
 
@@ -47,14 +54,14 @@ const storage = multer.diskStorage({
 /*  */
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
-
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 
 
 /* ROUTES */
 app.use("/auth", authRoutes);   //  login auth
 app.use("/users", userRoutes);
-
+app.use("/posts", postRoutes);
 
 
 
