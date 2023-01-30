419b0d652ecf2cbd411b885877ffceb87781676e
Sandivsv <sandeepkmr7379@gmail.com>
2023-01-30 10:26:37 +0530

    Navbar section implemented

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import Navbar from "../navbar";
+
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <Box>  
+        <Navbar/>
+    </Box>
   )
 }
 
